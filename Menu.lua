4ce765d28397a8ead24f19196261f287d9a7de36
ENDLESS-HACKER0 <84108554+ENDLESS-HACKER0@users.noreply.github.com>
2021-12-31 12:57:19 -0500

    Update Menu.lua

--- Menu.lua
+++ Menu.lua
@@ -150,7 +150,7 @@ fly.BackgroundTransparency = 1
 fly.Text = "Fly(X)"
 fly.TextScaled = true
 fly.Size = UDim2.new(0.1, 0, 0.08, 0)
-fly.Position = UDim2.new(0.38, 0, 0.02, 0)
+fly.Position = UDim2.new(0.36, 0, 0.02, 0)
 fly.TextColor3 = Color3.fromRGB(0, 255, 0)
 
 FirePets.BorderSizePixel = 0
@@ -158,7 +158,7 @@ FirePets.BackgroundTransparency = 1
 FirePets.Text = "Fire Pets"
 FirePets.TextScaled = true
 FirePets.Size = UDim2.new(0.1, 0, 0.08, 0)
-FirePets.Position = UDim2.new(0.5, 0, 0.02, 0)
+FirePets.Position = UDim2.new(0.4, 0, 0.02, 0)
 FirePets.TextColor3 = Color3.fromRGB(255, 115, 15)
 
 --Main Script
