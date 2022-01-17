8755be43142e71fb7cfe0cf77e6a044773a0abc8
ENDLESS-HACKER0 <84108554+ENDLESS-HACKER0@users.noreply.github.com>
2022-01-17 18:38:41 -0500

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -6,30 +6,34 @@ for _,player in pairs(game.Players:GetPlayers()) do
 	if player.UserId == LP.UserId then
 		print("Skipped Local Player!")
 	elseif player.Character:FindFirstChild("HumanoidRootPart") then
-		local copyBG = bg:Clone()
-		local copyBG2 = bg:Clone()
-		local copyFrame = frame:Clone()
-		local copyFrame2 = frame:Clone()
-		copyBG.AlwaysOnTop = true
-		copyBG2.AlwaysOnTop = true
-		copyBG.Parent = player.Character.HumanoidRootPart
-		copyBG2.Parent = copyBG.Parent
-		copyFrame.Parent = copyBG
-		copyFrame2.Parent = copyBG2
-		copyFrame.BackgroundTransparency = 0.5
-		copyFrame.Size = UDim2.new(1, 0, 1, 0)
-		copyFrame2.Size = UDim2.new(1, 0, 1, 0)
-		copyBG.Size = UDim2.new(0, 50, 0, 50)
-		copyBG2.Size = UDim2.new(0, 10, 0, 10)
-		copyBG.Name = "copyBackground"
-		copyBG2.Name = "copyBackground2"
-		copyFrame.BackgroundTransparency = 0.5
-		copyFrame2.BackgroundTransparency = 0.5
-		copyFrame.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
-		copyFrame2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-		copyFrame.BorderSizePixel = 0
-		copyFrame2.BorderSizePixel = 0
-		print(tostring(player).." has ESP")
+		if player.Character.HumanoidRootPart:FindFirstChild("copyBackground") then
+			print(tostring(player).." already has ESP")
+		else
+			local copyBG = bg:Clone()
+			local copyBG2 = bg:Clone()
+			local copyFrame = frame:Clone()
+			local copyFrame2 = frame:Clone()
+			copyBG.AlwaysOnTop = true
+			copyBG2.AlwaysOnTop = true
+			copyBG.Parent = player.Character.HumanoidRootPart
+			copyBG2.Parent = copyBG.Parent
+			copyFrame.Parent = copyBG
+			copyFrame2.Parent = copyBG2
+			copyFrame.BackgroundTransparency = 0.5
+			copyFrame.Size = UDim2.new(1, 0, 1, 0)
+			copyFrame2.Size = UDim2.new(1, 0, 1, 0)
+			copyBG.Size = UDim2.new(0, 50, 0, 50)
+			copyBG2.Size = UDim2.new(0, 10, 0, 10)
+			copyBG.Name = "copyBackground"
+			copyBG2.Name = "copyBackground2"
+			copyFrame.BackgroundTransparency = 0.5
+			copyFrame2.BackgroundTransparency = 0.5
+			copyFrame.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+			copyFrame2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+			copyFrame.BorderSizePixel = 0
+			copyFrame2.BorderSizePixel = 0
+			print("Added ESP to "..tostring(player))
+		end
 	else
 		print(tostring(player).." did not have a HumanoidRootPart")
 	end
