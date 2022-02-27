762c39bb1535da1091fc4257cf9bfac11cf3e912
ENDLESS-HACKER0 <84108554+ENDLESS-HACKER0@users.noreply.github.com>
2022-02-27 18:04:13 -0500

    Update API.lua

--- API.lua
+++ API.lua
@@ -24,24 +24,62 @@ end
 
 function API:ToGame(place)
 	local TeleportService = game:GetService("TeleportService")
-	
+
 	TeleportService:Teleport(place, LP)
 end
 
-function API:track(player)
-	local line  = Instance.new("Beam")
-	local A0 = Instance.new("Attachment")
-	local A1 = Instance.new("Attachment")
-	
-	A0.Parent = LP.Character.HumanoidRootPart
-	A1.Parent = game.Players[player].Character:WaitForChild("HumanoidRootPart")
-	
-	line.Parent = workspace
-	line.Attachment0 = A0
-	line.Attachment1 = A1
-	line.Width0 = 0.1
-	line.Width1 = 0.1
-	line.FaceCamera = true
+function API:trace(Player)
+	if Player == "All" then
+		for _,person in pairs(game.Players:GetPlayers()) do
+			if person.UserId == LP.UserId then
+				print("Skipped Local Player!")
+			elseif person.Character:FindFirstChild("HumanoidRootPart") then
+				if person.Character.HumanoidRootPart:FindFirstChild("Inset Tracer") then
+					print(tostring(person).." already has tracer")
+				else
+					local line  = Instance.new("Beam")
+					local A0 = Instance.new("Attachment")
+					local A1 = Instance.new("Attachment")
+
+					A0.Parent = LP.Character.HumanoidRootPart
+					A1.Parent = person.Character.HumanoidRootPart
+					
+					line.Name = "Inset Tracer"
+					line.Parent = workspace
+					line.Attachment0 = A0
+					line.Attachment1 = A1
+					line.Width0 = 0.5
+					line.Width1 = 0.5
+					line.FaceCamera = true
+				end
+			else
+				print(tostring(person).." did not have a HumanoidRootPart")
+			end
+		end
+	else
+		if game.Players[Player].Character:FindFirstChild("HumanoidRootPart") then
+			if game.Players[Player].Character:FindFirstChild("Inset Tracer") then
+				print(Player.." already has a tracer")
+			else
+				local line  = Instance.new("Beam")
+				local A0 = Instance.new("Attachment")
+				local A1 = Instance.new("Attachment")
+
+				A0.Parent = LP.Character.HumanoidRootPart
+				A1.Parent = game.Players[Player].Character:WaitForChild("HumanoidRootPart")
+
+				line.Name = "Inset Tracer"
+				line.Parent = workspace
+				line.Attachment0 = A0
+				line.Attachment1 = A1
+				line.Width0 = 0.5
+				line.Width1 = 0.5
+				line.FaceCamera = true
+			end
+		else
+			print(Player.." did not have a HumanoidRootPart")
+		end
+	end
 end
 
 return API
