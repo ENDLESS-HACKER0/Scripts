7b65c028df6f12fbbbdae4e451be290c70b22c51
ENDLESS-HACKER0 <84108554+ENDLESS-HACKER0@users.noreply.github.com>
2022-02-26 22:41:49 -0500

    Update API.lua

--- API.lua
+++ API.lua
@@ -19,7 +19,7 @@ function API:fire(color, color2)
 end
 
 function API:unfire()
-	LP.Character.Head:WaitForChild("Inset Fire")
+	LP.Character:WaitForChild("Head"):WaitForChild("Inset Fire"):Destroy()
 end
 
 return API
