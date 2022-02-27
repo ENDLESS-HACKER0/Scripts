b4f2468845840dc85b88299a70a394538c661956
ENDLESS-HACKER0 <84108554+ENDLESS-HACKER0@users.noreply.github.com>
2022-02-26 22:35:15 -0500

    Update API.lua

--- API.lua
+++ API.lua
@@ -9,4 +9,17 @@ function API:reset()
 	hum.Health = 0
 end
 
+function API:fire(color, color2)
+	local fire = Instance.new("Fire")
+	
+	fire.Parent = LP.Character:WaitForChild("Head")
+	fire.Color = color
+	fire.SecondaryColor = color2
+	fire.Name = "Inset Fire"
+end
+
+function API:unfire()
+	LP.Character:WaitForChild("Head"):WaitForChild("Inset Fire")
+end
+
 return API
