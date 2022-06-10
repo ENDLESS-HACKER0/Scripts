98b94cd09818146f8ddb6b3c8950beae115a53bd
ENDLESS-HACKER0 <84108554+ENDLESS-HACKER0@users.noreply.github.com>
2022-06-09 20:00:56 -0400

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -7,82 +7,14 @@ for _,player in pairs(game.Players:GetPlayers()) do
 		warn("Local Player Error")
 	elseif player.Character:FindFirstChild("HumanoidRootPart") then
 		if player.Character.HumanoidRootPart:FindFirstChild("copyBackground") then
-			print(tostring(player).." already has ESP")
-		else
-			if player.Character:FindFirstChild("Head") then
-				if player.Team ~= nil then
-					if player.Team ~= LP.Team then
-						local copyBG = bg:Clone()
-						local copyBG2 = bg:Clone()
-						local copyBG3 = bg:Clone()
-						local copyFrame = frame:Clone()
-						local copyFrame2 = frame:Clone()
-						local copyFrame3 = frame:Clone()
-						copyBG.AlwaysOnTop = true
-						copyBG2.AlwaysOnTop = true
-						copyBG3.AlwaysOnTop = false
-						copyBG.Parent = player.Character.HumanoidRootPart
-						copyBG2.Parent = copyBG.Parent
-						copyBG3.Parent = player.Character.Head
-						copyFrame.Parent = copyBG
-						copyFrame2.Parent = copyBG2
-						copyFrame3.Parent = copyBG3
-						copyFrame.Size = UDim2.new(1, 0, 1, 0)
-						copyFrame2.Size = UDim2.new(1, 0, 1, 0)
-						copyFrame3.Size = UDim2.new(1, 0, 1, 0)
-						copyBG.Size = UDim2.new(0, 50, 0, 50)
-						copyBG2.Size = UDim2.new(0, 10, 0, 10)
-						copyBG3.Size = UDim2.new(0, 30, 0, 30)
-						copyBG.Name = "copyBackground"
-						copyBG2.Name = "copyBackground2"
-						copyBG2.Name = "copyBackground3"
-						copyFrame.BackgroundTransparency = 0.5
-						copyFrame2.BackgroundTransparency = 0.5
-						copyFrame3.BackgroundTransparency = 0
-						copyFrame.BackgroundColor3 = Color3.fromRGB(100, 0, 0)
-						copyFrame2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-						copyFrame3.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
-						copyFrame.BorderSizePixel = 0
-						copyFrame2.BorderSizePixel = 0
-						copyFrame3.BorderSizePixel = 0
-						print("Added Other Team ESP to "..tostring(player))
-					elseif player.Team == LP.Team then
-						local copyBG = bg:Clone()
-						local copyBG2 = bg:Clone()
-						local copyBG3 = bg:Clone()
-						local copyFrame = frame:Clone()
-						local copyFrame2 = frame:Clone()
-						local copyFrame3 = frame:Clone()
-						copyBG.AlwaysOnTop = true
-						copyBG2.AlwaysOnTop = true
-						copyBG3.AlwaysOnTop = false
-						copyBG.Parent = player.Character.HumanoidRootPart
-						copyBG2.Parent = copyBG.Parent
-						copyBG3.Parent = player.Character.Head
-						copyFrame.Parent = copyBG
-						copyFrame2.Parent = copyBG2
-						copyFrame3.Parent = copyBG3
-						copyFrame.Size = UDim2.new(1, 0, 1, 0)
-						copyFrame2.Size = UDim2.new(1, 0, 1, 0)
-						copyFrame3.Size = UDim2.new(1, 0, 1, 0)
-						copyBG.Size = UDim2.new(0, 50, 0, 50)
-						copyBG2.Size = UDim2.new(0, 10, 0, 10)
-						copyBG3.Size = UDim2.new(0, 30, 0, 30)
-						copyBG.Name = "copyBackground"
-						copyBG2.Name = "copyBackground2"
-						copyBG2.Name = "copyBackground3"
-						copyFrame.BackgroundTransparency = 0.5
-						copyFrame2.BackgroundTransparency = 0.5
-						copyFrame3.BackgroundTransparency = 0
-						copyFrame.BackgroundColor3 = Color3.fromRGB(0, 255, 106)
-						copyFrame2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-						copyFrame3.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
-						copyFrame.BorderSizePixel = 0
-						copyFrame2.BorderSizePixel = 0
-						copyFrame3.BorderSizePixel = 0
-						print("Added Team ESP to "..tostring(player))
-					end
-				else
+			print("Updating "..tostring(player).."'s ESP")
+			player.Character.HumanoidRootPart:FindFirstChild("copyBackground"):Destroy()
+			player.Character.HumanoidRootPart:FindFirstChild("copyBackground2"):Destroy()
+			player.Character.HumanoidRootPart:FindFirstChild("copyBackground3"):Destroy()
+		end
+		if player.Character:FindFirstChild("Head") then
+			if player.Team ~= nil then
+				if player.Team ~= LP.Team then
 					local copyBG = bg:Clone()
 					local copyBG2 = bg:Clone()
 					local copyBG3 = bg:Clone()
@@ -110,17 +42,87 @@ for _,player in pairs(game.Players:GetPlayers()) do
 					copyFrame.BackgroundTransparency = 0.5
 					copyFrame2.BackgroundTransparency = 0.5
 					copyFrame3.BackgroundTransparency = 0
-					copyFrame.BackgroundColor3 = Color3.fromRGB(0, 150, 0)
+					copyFrame.BackgroundColor3 = Color3.fromRGB(100, 0, 0)
 					copyFrame2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
 					copyFrame3.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
 					copyFrame.BorderSizePixel = 0
 					copyFrame2.BorderSizePixel = 0
 					copyFrame3.BorderSizePixel = 0
-					print("Added ESP to "..tostring(player))
+					print("Added Other Team ESP to "..tostring(player))
+				elseif player.Team == LP.Team then
+					local copyBG = bg:Clone()
+					local copyBG2 = bg:Clone()
+					local copyBG3 = bg:Clone()
+					local copyFrame = frame:Clone()
+					local copyFrame2 = frame:Clone()
+					local copyFrame3 = frame:Clone()
+					copyBG.AlwaysOnTop = true
+					copyBG2.AlwaysOnTop = true
+					copyBG3.AlwaysOnTop = false
+					copyBG.Parent = player.Character.HumanoidRootPart
+					copyBG2.Parent = copyBG.Parent
+					copyBG3.Parent = player.Character.Head
+					copyFrame.Parent = copyBG
+					copyFrame2.Parent = copyBG2
+					copyFrame3.Parent = copyBG3
+					copyFrame.Size = UDim2.new(1, 0, 1, 0)
+					copyFrame2.Size = UDim2.new(1, 0, 1, 0)
+					copyFrame3.Size = UDim2.new(1, 0, 1, 0)
+					copyBG.Size = UDim2.new(0, 50, 0, 50)
+					copyBG2.Size = UDim2.new(0, 10, 0, 10)
+					copyBG3.Size = UDim2.new(0, 30, 0, 30)
+					copyBG.Name = "copyBackground"
+					copyBG2.Name = "copyBackground2"
+					copyBG2.Name = "copyBackground3"
+					copyFrame.BackgroundTransparency = 0.5
+					copyFrame2.BackgroundTransparency = 0.5
+					copyFrame3.BackgroundTransparency = 0
+					copyFrame.BackgroundColor3 = Color3.fromRGB(0, 255, 106)
+					copyFrame2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+					copyFrame3.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
+					copyFrame.BorderSizePixel = 0
+					copyFrame2.BorderSizePixel = 0
+					copyFrame3.BorderSizePixel = 0
+					print("Added Team ESP to "..tostring(player))
 				end
 			else
-				warn(tostring(player).." had a HumanoidRootPart but not a Head")
+				local copyBG = bg:Clone()
+				local copyBG2 = bg:Clone()
+				local copyBG3 = bg:Clone()
+				local copyFrame = frame:Clone()
+				local copyFrame2 = frame:Clone()
+				local copyFrame3 = frame:Clone()
+				copyBG.AlwaysOnTop = true
+				copyBG2.AlwaysOnTop = true
+				copyBG3.AlwaysOnTop = false
+				copyBG.Parent = player.Character.HumanoidRootPart
+				copyBG2.Parent = copyBG.Parent
+				copyBG3.Parent = player.Character.Head
+				copyFrame.Parent = copyBG
+				copyFrame2.Parent = copyBG2
+				copyFrame3.Parent = copyBG3
+				copyFrame.Size = UDim2.new(1, 0, 1, 0)
+				copyFrame2.Size = UDim2.new(1, 0, 1, 0)
+				copyFrame3.Size = UDim2.new(1, 0, 1, 0)
+				copyBG.Size = UDim2.new(0, 50, 0, 50)
+				copyBG2.Size = UDim2.new(0, 10, 0, 10)
+				copyBG3.Size = UDim2.new(0, 30, 0, 30)
+				copyBG.Name = "copyBackground"
+				copyBG2.Name = "copyBackground2"
+				copyBG2.Name = "copyBackground3"
+				copyFrame.BackgroundTransparency = 0.5
+				copyFrame2.BackgroundTransparency = 0.5
+				copyFrame3.BackgroundTransparency = 0
+				copyFrame.BackgroundColor3 = Color3.fromRGB(0, 150, 0)
+				copyFrame2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+				copyFrame3.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
+				copyFrame.BorderSizePixel = 0
+				copyFrame2.BorderSizePixel = 0
+				copyFrame3.BorderSizePixel = 0
+				print("Added ESP to "..tostring(player))
 			end
+		else
+			warn(tostring(player).." had a HumanoidRootPart but not a Head")
 		end
 	else
 		warn(tostring(player).." did not have a HumanoidRootPart")
